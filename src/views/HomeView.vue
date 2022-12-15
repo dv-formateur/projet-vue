f6ed17e8f359a75df6c0114ccffb3733b3943e97
dv-formateur <dvermonden@yahoo.fr>
2022-12-15 22:07:17 +0100

    Updated main message

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Test View Automatic Deploy with Github Actions" />
+    <HelloWorld msg="Test Vue Automatic Deploy with Github Actions" />
   </div>
 </template>
 
