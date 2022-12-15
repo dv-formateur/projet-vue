6eb1c4db64c8339bd3ca3cebf33215f468b1cfcd
dv-formateur <dvermonden@yahoo.fr>
2022-12-15 21:25:50 +0100

    Update cd

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/projet-vue/" : "/",
+  // publicPath: process.env.NODE_ENV === "production" ? "/projet-vue/" : "/",
   transpileDependencies: true,
   outputDir: "docs",
+  base: "/projet-vue/",
 });
